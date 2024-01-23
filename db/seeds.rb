43931bae93e78c817d9abe08c910868f52bcecfd
Justine Auguste <justine_auguste@hotmail.com>
2024-01-23 16:01:09 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  article = Article.new(title: Faker::Company.bs, content: Faker::Markdown.emphasis)
+  article.save
+end
